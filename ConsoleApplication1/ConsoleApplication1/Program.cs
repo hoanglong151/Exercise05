98ae29490e06a9abedfb19a1ffb81193f59a2823
hoanglong <nguyenphamhoanglong2000@gmail.com>
2019-10-08 17:54:15 +0700

    Exercise05a

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,74 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(IsLower('f'));
+            Print_a_2_z_v1();
+            Print_a_2_z_v2();
+            Print_a_2_z_v3();
+            {
+                while(true)
+                {
+            char c = Console.ReadKey().KeyChar;
+            Console.WriteLine(Char2code(c));
+                }   
         }
+            
+            
+        }
+        public static void Print_a_2_z_v1()
+        {
+            char c = ' ';
+            for(int i = 97 ; i < 122;i++)
+            {
+                c = (char)i;
+                Console.Write(c);
+            }
+        }
+        public static void Print_a_2_z_v2()
+        {
+            char c = 'a';
+            for (int i = 0; i < 26; i++)
+            {
+                Console.Write(c);
+                c++;
+            }
+        }
+        public static void Print_a_2_z_v3()
+        {
+            String s = "abcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i < 26; i++)
+            {
+                Console.Write(s[i]);
+                
+            }
+        }
+
+        public static int Char2code(char c)
+        {
+            return (int)c;
+        }
+        public static bool IsLower(char c)
+        {
+            
+            for (int i = 65; i <= 90; i++)
+            {
+                if(Char2code == c[i])
+                return true;
+            }
+            return false;
+        }
+        public static bool IsUpper(char c)
+        {
+            return false;
+        }
+        public static char ToUpper(char c)
+        {
+            return c;
+        }
+        public static char ToLower(char c)
+        {
+            return c;
+        }
+        
     }
 }
